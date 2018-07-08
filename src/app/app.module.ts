03ce5d1d7c056131f3ce430cb52569c9b6c12969
xcshanto <shanto906@gmail.com>
2018-07-08 15:52:34 +0600

    Different Components were Created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,28 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { Observable } from 'rxjs';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { CartComponent } from './cart/cart.component';
+import { OrderComponent } from './order/order.component';
+import { CustomerOrdersComponent } from './customer-orders/customer-orders.component';
+import { CheckoutBillComponent } from './checkout-bill/checkout-bill.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    CartComponent,
+    OrderComponent,
+    CustomerOrdersComponent,
+    CheckoutBillComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/checkout-bill/checkout-bill.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-bill',
+  templateUrl: './checkout-bill.component.html',
+  styleUrls: ['./checkout-bill.component.css']
+})
+export class CheckoutBillComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
